f8e3bd9995c59eced586ec8bd28e62a68b6f207e
Markus Felten <Markus.Felten@gmx.de>
2020-11-28 20:58:16 +0100

    wip: deliver several sequenzes

--- module.js
+++ module.js
@@ -1,22 +1,35 @@
-/**
- * Returns a promise which resolves after time had passed.
- */
-const delay = time => new Promise(resolve => setTimeout(resolve, time));
-
-async function* delayedRange(max) {
-  for (let i = 0; i < max; i++) {
-    await delay(1000);
-    yield i;
-  }
-}
-
-console.log('example start');
-
 async function worker() {
-  for await (let number of delayedRange(10)) {
-    //console.log(number);
-    alert(number);
+  const init = initializeModel();
+
+  for await (const s of init.a) {
+    console.log(s);
   }
 }
 
 worker();
+
+
+function initializeModel()
+{
+  const a = sequence("a");
+  const b = sequence("b");
+
+  return {
+    a,
+    b
+  };
+}
+
+async function* sequence(name, time = 100, num = 10, errIndex = -1) {
+  for (let i = 0; i < num; i += 1) {
+    yield new Promise((resolve, reject) =>
+      setTimeout(() => {
+        if (i === errIndex) {
+          reject(name + i);
+        } else {
+          resolve(name + i);
+        }
+      }, time)
+    );
+  }
+}
